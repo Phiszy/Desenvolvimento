a0e9af64c4becc2467989be3196b648c3114f7d2
Phiszy <pedrohenriquecbock@gmail.com>
2023-03-08 12:01:49 -0300

    como conectar com o backend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
-import {Header} from "../components/Header.jsx"
+import { Header } from "../components/Header.jsx";
+import axios from "axios";
+import { useQuery } from "react-query"
 
+export function Login() {
+    //try é dentro da função
+    try {
+        const { data, isLoading, isError, error } = useQuery("response", async () => {
 
-export function Login(){
-    return(
+            const resp = await axios.get(async () => {
+                "http//:localhost:3005/something"
+            })
+            return resp
+            
+        })
+        if (isLoading) {
+            return (<><p>{isLoading}...</p></>)
+        }
+        if (isError) {
+            return (<>
+                <p>{error.message}</p>
+            </>)
+        }
+
+        return data.resp.message
+    } catch (error) {
+        console.log(error);
+    }
+
+    return (
         <>
-        <div>
-            <Header /> 
-        </div>
+            <div>
+                <form>
+                    <input type="text" />
+                </form>
+            </div>
+
         </>
     )
 }
